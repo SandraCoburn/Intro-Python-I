c57e0791d6f4de1a26c6d0691937c411c58f91e1
Sandra Coburn <sandra_coburn@hotmail.com>
2020-07-06 12:21:23 -0700

    Finished 13, cleaned up 03

--- src/03_modules.py
+++ src/03_modules.py
@@ -6,34 +6,37 @@ level operating system functionality.
 """
 
 import sys
+import fileinput
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
 print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv[1:]))
-
+print('Argument List:', str(sys.argv))
+for arg in sys.argv:
+    print("arguments: ",arg)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-import os
-print(os.path)
+print("platform: ",sys.platform)
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-print(sys.version_info)
+print("version:",sys.version_info)
 
 
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
-
+import os
+print(os.path)
 # Print the current process ID
 # YOUR CODE HERE
+print("process id: ", os.getpid())
 print(os.ctermid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-print(os.getcwd())
+print("Working directory: ",os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
-print(os.getlogin())
+print("login: ",os.getlogin())
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+my_file = open("src/foo.txt")
+print(my_file.readlines())
+my_file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+# with open("bar.text", mode="a+") as new_file:
+#     text = new_file.writelines("This is a new file created with file IO.\n", "There are a few ways to write and read.\n", "To write you use mode=w and it will create a new file if one was not found")
+#     print(text)
+new_file = open("bar.text",'w') 
+new_file.write("This is a new file created with file io")
+print(new_file)
+new_file.close()
